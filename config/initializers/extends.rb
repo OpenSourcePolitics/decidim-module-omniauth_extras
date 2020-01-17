78a6751ee6d44d0b4decb6fc1954821f68dd5608
moustachu <git@moustachu.net>
2020-01-17 10:44:08 +0100

    fix extends require

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,3 +1,2 @@
 require "extends/decidim/omniauth_registration_form_extend"
 require "extends/decidim/devise/omniauth_registrations_controller_extend"
-require "extends/decidim/initiatives/initiative_helper_extend"
